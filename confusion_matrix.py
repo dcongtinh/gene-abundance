598c068110bd653dd72daa4a8beaaeb7d798d171
Tinh Dao <dcongtinh@gmail.com>
2020-02-27 16:52:22 +0700

    Added: confusion_matrix.py

---
+++ confusion_matrix.py
@@ -0,0 +1,90 @@
+import numpy as np
+
+
+def plot_confusion_matrix(cm,
+                          target_names=None,
+                          title='Confusion matrix',
+                          cmap=None,
+                          normalize=True,
+                          val_auc=None):
+    """
+    given a sklearn confusion matrix (cm), make a nice plot
+
+    Arguments
+    ---------
+    cm:           confusion matrix from sklearn.metrics.confusion_matrix
+
+    target_names: given classification classes such as [0, 1, 2]
+                  the class names, for example: ['high', 'medium', 'low']
+
+    title:        the text to display at the top of the matrix
+
+    cmap:         the gradient of the values displayed from matplotlib.pyplot.cm
+                  see http://matplotlib.org/examples/color/colormaps_reference.html
+                  plt.get_cmap('jet') or plt.cm.Blues
+
+    normalize:    If False, plot the raw numbers
+                  If True, plot the proportions
+    
+    val_auc:      Value of Area Under Curve
+
+    Usage
+    -----
+    plot_confusion_matrix(cm           = cm,                  # confusion matrix created by
+                                                              # sklearn.metrics.confusion_matrix
+                          normalize    = True,                # show proportions
+                          target_names = y_labels_vals,       # list of names of the classes
+                          title        = best_estimator_name) # title of graph
+
+    Citiation
+    ---------
+    http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html
+
+    """
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import itertools
+
+    val_acc = np.trace(cm) / float(np.sum(cm))
+    val_loss = 1 - val_acc
+
+    if cmap is None:
+        cmap = plt.get_cmap('Blues')
+
+    plt.figure(figsize=(4, 2))
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title, pad=14)
+    plt.colorbar()
+
+    if target_names is not None:
+        tick_marks = np.arange(len(target_names))
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+
+
+    thresh = cm.max() / 1.5 if normalize else cm.max() / 2
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if normalize:
+            plt.text(j, i, "{:0.3f}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+        else:
+            plt.text(j, i, "{:,}".format(cm[i, j]),
+                     horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+
+    plt.tight_layout()
+    plt.ylabel('Predicted label')
+    plt.xlabel('True label\nval_acc={:0.3f};val_auc={:0.3f}'.format(val_acc, val_auc))
+    plt.show()
+
+plot_confusion_matrix(cm           = np.array([[ 10.11, 1.29,],
+                                              [  1.69, 10.11],]), 
+                      normalize    = False,
+                      target_names = ['patient', 'control'],
+                      title        = "Average Confusion matrix of Cirgene",
+                      val_auc      = 0.941)
